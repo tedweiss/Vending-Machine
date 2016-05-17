829f68a3e284bd48654946404fa3b69f6f4a04b8
Ted Weiss <theodore_weiss@outlook.com>
2016-05-16 21:12:51 -0400

    check if total is met

--- vending-machine.js
+++ vending-machine.js
@@ -9,15 +9,25 @@ function chooseFood(food) {
     console.log("You chose " + food.type + " and you owe " + food.price);
     total = food.price;
 }
-
 chooseFood(food[0]);
 
 function insertCoin(coin) {
     if (coin === 25 || coin === 10 || coin === 5) {
         total = total -= coin;
-        console.log("You still owe: " + total);
+        metTotal();
     } else {
         console.log("Please enter a valid coin.");
+    }
+}
+
+function metTotal() {
+    if (total > 0) {
+        console.log("You still owe: " + total);
+    } else if (total < 0) {
+        total = Math.abs(total);
+        console.log("Here is your change: " + total);
+    } else {
+        console.log("Thank you! Enjoy your snack!");
     }
 }
 
@@ -25,3 +35,6 @@ insertCoin(25);
 insertCoin(10);
 insertCoin(5);
 insertCoin(1);
+insertCoin(25);
+insertCoin(25);
+insertCoin(25);
