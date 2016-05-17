ff44bd192ef2fb8083521306296ab5c99b4b0066
Ted Weiss <theodore_weiss@outlook.com>
2016-05-16 21:06:26 -0400

    select food

--- vending-machine.js
+++ vending-machine.js
@@ -1,12 +1,23 @@
+var food = [
+    { type: "cola", price: 100 },
+    { type: "chips", price: 50 },
+    { type: "candy", price: 65 }
+];
+
+function chooseFood(food) {
+    console.log("You chose " + food.type + " and you owe " + food.price);
+}
+chooseFood(food[0]);
+
 var total = 0;
 
 function insertCoin(coin) {
 
-	if(coin === 25 || coin === 10 || coin === 5){
-		console.log(total += coin);
-	} else {
-    console.log("Please enter a valid coin. Here is your coin back.");
-  }
+    if (coin === 25 || coin === 10 || coin === 5) {
+        console.log(total += coin);
+    } else {
+        console.log("Please enter a valid coin. Here is your coin back.");
+    }
 }
 
 insertCoin(25);
