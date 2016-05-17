f9953ad08d215d96eb9d83fd326dc62bd69aacaa
Ted Weiss <theodore_weiss@outlook.com>
2016-05-16 21:27:39 -0400

    don't subtract quantity until given snack

--- vending-machine.js
+++ vending-machine.js
@@ -1,5 +1,6 @@
 var total = 0;
 var acceptedCoins = [];
+var selectedFood = [];
 var food = [
     { type: "cola", price: 100, quantity: 2 },
     { type: "chips", price: 50, quantity: 5 },
@@ -10,10 +11,21 @@ function chooseFood(food) {
     if (food.quantity <= 0) {
         console.log(food.type + " is SOLD OUT. Please make another selection.");
     } else {
-        food.quantity = food.quantity - 1;
-        console.log("There is " + food.quantity + " " + food.type + " left.");
+        selectedFood.push(food);
+        console.log(selectedFood[0].type + " was selected");
+        console.log("There are " + food.quantity + " " + food.type + " left.");
         console.log("You chose " + food.type + " and you owe " + food.price);
         total = food.price;
+    }
+}
+
+function checkSelectedFood() {
+    for (i = 0; i < food.length; i++) {
+        if (selectedFood[0] === food[i]) {
+            console.log(selectedFood[0].type + " and " + food[i].type + " match");
+            food[i].quantity = food[i].quantity - 1;
+            console.log(food[i].type + " now has " + food[i].quantity);
+        }
     }
 }
 
@@ -37,10 +49,14 @@ function metTotal() {
     } else if (total < 0) {
         total = Math.abs(total);
         console.log("Thank you! Enjoy your snack! Here is your change: " + total);
+        checkSelectedFood();
+        selectedFood = [];
         acceptedCoins = [];
         total = 0;
     } else {
         console.log("Thank you! Enjoy your snack!");
+        checkSelectedFood();
+        selectedFood = [];
     }
 }
 
@@ -52,9 +68,9 @@ function returnCoins() {
         for (i = 0; i < acceptedCoins.length; i++) {
             console.log("Here is your coin back: " + acceptedCoins[i]);
         }
+        selectedFood = [];
         acceptedCoins = [];
         total = 0;
-        console.log(acceptedCoins);
         console.log("Please make a selection.");
         console.log("INSERT COIN");
     }
@@ -69,10 +85,10 @@ function checkSelection() {
 chooseFood(food[2]);
 chooseFood(food[1]);
 
-insertCoin(25);
 insertCoin(10);
-insertCoin(5);
 insertCoin(1);
+insertCoin(5);
+insertCoin(25);
 insertCoin(25);
 
 chooseFood(food[1]);
@@ -81,11 +97,18 @@ insertCoin(10);
 
 returnCoins();
 
-chooseFood(food[0]);
+chooseFood(food[1]);
 
 insertCoin(5);
 insertCoin(25);
 insertCoin(25);
 insertCoin(25);
+
+chooseFood(food[0]);
+
 insertCoin(10);
 insertCoin(10);
+insertCoin(5);
+insertCoin(25);
+insertCoin(25);
+insertCoin(25);
