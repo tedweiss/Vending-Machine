3ea2b4d9d51fe9ebb9f0c276cd19947869242ea0
Ted Weiss <theodore_weiss@outlook.com>
2016-05-16 21:15:37 -0400

    created accepted coins array, return accepted coins

--- vending-machine.js
+++ vending-machine.js
@@ -1,4 +1,5 @@
 var total = 0;
+var acceptedCoins = [];
 var food = [
     { type: "cola", price: 100 },
     { type: "chips", price: 50 },
@@ -9,11 +10,12 @@ function chooseFood(food) {
     console.log("You chose " + food.type + " and you owe " + food.price);
     total = food.price;
 }
-chooseFood(food[0]);
 
 function insertCoin(coin) {
     if (coin === 25 || coin === 10 || coin === 5) {
         total = total -= coin;
+        acceptedCoins.push(coin);
+        console.log(acceptedCoins);
         metTotal();
     } else {
         console.log("Please enter a valid coin.");
@@ -31,10 +33,29 @@ function metTotal() {
     }
 }
 
+function returnCoins() {
+    console.log("Returning Coins");
+    for (i = 0; i < acceptedCoins.length; i++) {
+        console.log("Here is your coin back: " + acceptedCoins[i]);
+    }
+    acceptedCoins = [];
+    total = 0;
+    console.log(acceptedCoins);
+    console.log("Please make a selection.");
+    console.log("INSERT COIN");
+}
+
+chooseFood(food[0]);
+
 insertCoin(25);
 insertCoin(10);
 insertCoin(5);
 insertCoin(1);
 insertCoin(25);
 insertCoin(25);
-insertCoin(25);
+
+returnCoins();
+
+chooseFood(food[2]);
+
+insertCoin(5);
